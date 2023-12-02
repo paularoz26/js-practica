10e27d73cd91b83649d6f91d6fd6d08ca498057c
paula <145855211+paularoz26@users.noreply.github.com>
2023-12-02 18:40:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+var variable
+
+constdatodelusuario=prompt ("Ingrese su NOMBRE y APELLIDO")
+const Bienvenidoalapagina='BIENVENIDO A LA PAGINA'
+const frase='¡'+ Bienvenidoalapagina +' ' +'!'
+console.log(frase)
+const tienetatuaje='si'
+const edad=parseInt
+(prompt("Ingrese su edad:"))
+if(edad>=18){true
+    console.log("BUENISIMOO! podes tatuarte")}
+else{console.log("EY!! NO podes tatuarte")}
+const tatuaje=prompt("TIENES TATUAJES HECHOS!?")//si
+if(tienetatuaje==tatuaje){"Que modelo buscas!!?"}
+
+
+
+const fecha=prompt("Ingrese el DIA y HORARIO")
+if(fecha=='domingo')
+console.log("LOCAL CERRADO!!")
+else{console.log("LOCAL ABIERTO!!!")}
+if(fecha=='lunes=13;30hs','lunes=15:30hs', 'lunes=17:30hs', 'lunes=19:30hs')
+console.log("DIAS DISPONIBLE")
+if(fecha=='martes','miercoles','jueves','viernes','sabado')
+console.log("DIAS NO DISPONIBLE")
+
+let tatuajes=parseInt
+(prompt("ingrese1 sin color, ingrese2 combinacion de 2 o mas sin color, ingrese3 con color, ingrese4 combinacion de 2 o mas con color, otro numero para salir"))
+switch (tatuajes)
+{case 1:console.log("SIN COLOR:")
+break
+case 2:console.log("COMBINACION DE 2 O MAS SIN COLOR:")
+break
+case 3:console.log("CON COLOR:")
+break
+case 4:console.log("COMBINACION DE 2 O MAS CON COLORES")
+break
+default:console.log("cancelar el pedido!!")
+break
+}
+
+
+
+let resultadototal=
+prompt ("Desea combinar tatuajes? (si/no)")
+if (resultadototal=="si")
+{continuar=false
+console.log("Genial")
+
+}
+console.log ("PRECIO SIN COLOR")
+var precioTotal =8000;
+var cantidad = 1;
+var precioPorUnidad = precioTotal * cantidad;
+console.log("El precio por unidad es: " + precioPorUnidad);
+var precioTotal =8000;
+for (var i =1; i <= 1; i++) {
+  var precioPorUnidad = precioTotal * i;
+  console.log("El precio por unidad de " + i + " unidades es: " + precioPorUnidad);
+
+}
+
+
+console.log ("PRECIO DE COMBINACION DE TATUAJES SIN COLOR")
+var precioTotal =8000;
+var cantidad = 1;
+var precioPorUnidad = precioTotal * cantidad;
+console.log("El precio por unidad es: " + precioPorUnidad);
+var precioTotal =16000;
+for (var i =2; i <= 10; i++) {
+  var precioPorUnidad = precioTotal * i;
+  console.log("El precio por unidad de " + i + " unidades es: " + precioPorUnidad);
+
+}
+console.log ("PRECIO CON COLOR")
+var precioTotal =10000;
+var cantidad = 1;
+var precioPorUnidad = precioTotal * cantidad;
+console.log("El precio por unidad es: " + precioPorUnidad);
+var precioTotal =10000;
+for (var i =1; i <=1; i++) {
+  var precioPorUnidad = precioTotal * i;
+  console.log("El precio por unidad de " + i + " unidades es: " + precioPorUnidad);
+
+}
+
+console.log ("PRECIO DE 2 COLORES O MAS")
+var precioTotal =12000;
+var cantidad = 1;
+var precioPorUnidad = precioTotal * cantidad;
+console.log("El precio por unidad es: " + precioPorUnidad);
+var precioTotal =12000;
+for (var i =2; i <= 10; i++) {
+  var precioPorUnidad = precioTotal * i;
+  console.log("El precio por unidad de " + i + " unidades es: " + precioPorUnidad);
+
+}
+
+
+
+
+
